be7daf93d5b77ecb392f4774286506c958b76548
juju773 <ju.li1@hotmail.fr>
2022-03-15 09:07:57 +0100

    remodif

--- src/main/kotlin/com/spendyourtime/Sever.kt
+++ src/main/kotlin/com/spendyourtime/Sever.kt
@@ -1,6 +1,5 @@
 package com.spendyourtime
 
-import com.usthe.sureness.DefaultSurenessConfig
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
 import org.slf4j.LoggerFactory
@@ -10,7 +9,7 @@ object Server {
     class Position(var x: Int, var y: Int)
     class Skin(var body: Int, var accesories: Int, var hairstyle: Int, var eyes: Int, var outfit: Int)
     class User(var email: String, var pseudo: String, var password: String, var skin: Skin)
-    class player(var id: Int, var user: User, var position: Position)
+    class Player(var id: Int, var user: User, var position: Position)
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -32,29 +31,29 @@ object Server {
             //USER REGISTER + LOGIN
             path("User") {
                 post("register") { ctx ->
-                    logger.info(ctx.body())
+                    logger.info("POST_REGISTER")
                     ctx.status(501)
                 }
 
                 post("login") { ctx ->
-                    logger.info(ctx.body())
+                    logger.info("POST_LOGIN")
                     ctx.status(501)
                 }
 
                 post("name") { ctx ->
-                    logger.info(ctx.body())
+                    logger.info("POST_NAME")
                     ctx.status(501)
                 }
 
                 get("skin") { ctx ->
-                    logger.info(ctx.body())
+                    logger.info("GET_SKIN")
                     ctx.status(501)
                 }
             }
 
             //Player
                 app.post("/Player/position") { ctx ->
-                    logger.info(ctx.body())
+                    logger.info("GET_PLAYER_POSITION")
                     ctx.status(501).json(ctx.status(501))
                 }
 
