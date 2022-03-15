7760a8ab226fb67588e679f1fadea95d0e09aba6
stheren <silvain.therene@gmail.com>
2022-03-15 11:16:38 +0100

    [Feat] Update Skin & Certification

--- src/main/kotlin/com/spendyourtime/ServerTest.kt
+++ src/main/kotlin/com/spendyourtime/ServerTest.kt
@@ -1,4 +1,21 @@
 package com.spendyourtime
 
-class ServerTest {
+import io.github.nefilim.kjwt.JWT
+import io.github.nefilim.kjwt.JWTKeyID
+import io.github.nefilim.kjwt.jwtDecodeString
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+object ServerTest {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val jwt = JWT.es256(JWTKeyID("kid-123")) {
+            claim("name", "Juju773")
+        }
+
+        var e = jwt.encode()
+
+        println(JWT.decode(e+"2"))
+    }
 }
---
+++ src/main/kotlin/com/spendyourtime/helpers/Certification.kt
@@ -0,0 +1,22 @@
+package com.spendyourtime.helpers
+
+import com.spendyourtime.data.Skin
+import com.spendyourtime.data.User
+import io.github.nefilim.kjwt.JWT
+import io.github.nefilim.kjwt.JWTKeyID
+
+object Certification {
+    fun create(u : User) : String{
+        val jwt = JWT.es256(JWTKeyID("kid-123")) {
+            claim("name", "Juju773")
+        }
+        return jwt.encode()
+    }
+
+    fun find(e : String) : User{
+        println(JWT.decode(e).tap {
+            println(it.claimValue("name"))
+        })
+        return User("Email", "Pseudo", "OK", Skin())
+    }
+}
