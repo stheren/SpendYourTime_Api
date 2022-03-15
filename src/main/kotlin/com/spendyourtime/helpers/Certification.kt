df6e5d18cca4dd7cff42cdd091a202759a28315a
stheren <silvain.therene@gmail.com>
2022-03-15 16:23:39 +0100

    [Feat] Update Certification.kt

--- src/main/kotlin/com/spendyourtime/helpers/Certification.kt
+++ src/main/kotlin/com/spendyourtime/helpers/Certification.kt
@@ -1,12 +1,10 @@
 package com.spendyourtime.helpers
 
 import arrow.core.handleError
-import arrow.core.left
-import arrow.core.leftIfNull
-import arrow.core.some
 import com.spendyourtime.data.User
 import io.github.nefilim.kjwt.JWT
 import io.github.nefilim.kjwt.JWTKeyID
+import io.javalin.http.Context
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -31,6 +29,17 @@ object Certification {
         }
         e.handleError {
             throw Exception("JWT Decode has an error.")
+        }
+    }
+
+    fun verification(ctx : Context, p : (User) -> Unit){
+        find(ctx.header("token").toString()) { user ->
+            if (user == null) {
+                ctx.json("DECODED_BUT_UNKNOW_PLAYER")
+                ctx.status(403)
+            } else {
+                p(user)
+            }
         }
     }
 }
