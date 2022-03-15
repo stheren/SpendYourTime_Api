9176cf44d0f61d5b433b8b2c53a34e8287934936
lpayet <lpayet0106@gmail.com>
2022-03-15 12:12:42 +0100

    User findby

--- src/main/kotlin/com/spendyourtime/data/User.kt
+++ src/main/kotlin/com/spendyourtime/data/User.kt
@@ -9,7 +9,20 @@ class User(var email: String, var pseudo: String, var password: String, var play
     init{
         if(allUsers.contains(this))
             throw Exception("Player already in allUsers")
+        for(user in allUsers){
+            if (user.pseudo == this.pseudo)
+                throw Exception("Pseudo already exists")
+            if(user.email == this.email)
+                throw Exception("Email already exists")
+        }
         allUsers.add(this)
+    }
+
+    fun findUserByPseudo(pseudo :String) : User?{
+        return allUsers.find { it.pseudo == pseudo }
+    }
+    fun findUserByEmail(email :String) : User?{
+        return allUsers.find { it.email == email }
     }
 
     override fun equals(other: Any?) : Boolean{
