fe4f75b4aabed165eda28d46b1affec4448df58e
stheren <silvain.therene@gmail.com>
2022-03-15 14:33:02 +0100

    [Feat] Update Certification.kt

--- src/main/kotlin/com/spendyourtime/ServerTest.kt
+++ src/main/kotlin/com/spendyourtime/ServerTest.kt
@@ -1,8 +1,15 @@
 package com.spendyourtime
 
+import com.spendyourtime.data.User
+import com.spendyourtime.helpers.Certification
+
 object ServerTest {
     @JvmStatic
     fun main(args: Array<String>) {
+        val e = Certification.create(User("Silvain", "Test", "ok"))
 
+        Certification.find(e){
+            println(it.email)
+        }
     }
 }
