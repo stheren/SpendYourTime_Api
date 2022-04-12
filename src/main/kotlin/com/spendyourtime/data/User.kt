5d42f56e7b33a067fc6e4e39e1806551c9bc3c1a
stheren <silvain.therene@gmail.com>
2022-04-12 14:41:22 +0200

    [Update] User.kt

--- src/main/kotlin/com/spendyourtime/data/User.kt
+++ src/main/kotlin/com/spendyourtime/data/User.kt
@@ -4,6 +4,16 @@ import com.spendyourtime.helpers.Database
 import com.spendyourtime.helpers.Sha512
 
 data class User(var email: String, var pseudo: String, var password: String, var player : Player = Player()){
+
+    companion object{
+        var autoincrement: Int = 0
+        get() {
+            field++
+            return field
+        }
+    }
+    // Create a autoincrement id
+    var id: Int = autoincrement
     init{
         password = Sha512.encode(password)
         if(Database.allUsers.contains(this))
