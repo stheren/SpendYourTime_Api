0067ea4d0dc4f7a5d27c88af5b55033e59513297
stheren <silvain.therene@gmail.com>
2022-04-14 16:31:45 +0200

    [Update] Message.kt

--- src/main/kotlin/com/spendyourtime/data/Message.kt
+++ src/main/kotlin/com/spendyourtime/data/Message.kt
@@ -1,3 +1,17 @@
 package com.spendyourtime.data
 
-class Message(val id: Int, val text: String, val user: User, val date: Long = System.currentTimeMillis())
+import com.spendyourtime.helpers.Database
+
+class Message(val text: String, val user: User, val date: Long = System.currentTimeMillis()){
+    companion object {
+        private fun getUniqueID(): Int {
+            var key = 0
+            while (Database.allChat.any { it.id == key }) {
+                key++
+            }
+            return key
+        }
+    }
+
+    var id: Int = getUniqueID()
+}
