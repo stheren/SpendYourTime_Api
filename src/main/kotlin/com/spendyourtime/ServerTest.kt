b7dc7e39633aa32525741b5ce4f23be00d19ea19
stheren <silvain.therene@gmail.com>
2022-04-24 19:53:49 +0200

    [Update] Remove ServerTest.kt

--- src/main/kotlin/com/spendyourtime/ServerTest.kt
+++ src/main/kotlin/com/spendyourtime/ServerTest.kt
@@ -1,21 +1,14 @@
 package com.spendyourtime
 
+import com.spendyourtime.data.Guild
+import com.spendyourtime.data.Message
 import com.spendyourtime.data.User
+import com.spendyourtime.data.Work.Companion.validateWork
 import com.spendyourtime.helpers.Certification
+import com.spendyourtime.helpers.Database
 
 object ServerTest {
     @JvmStatic
     fun main(args: Array<String>) {
-        val e = Certification.create(User("er", "er", "er"))
-
-        println(e)
-
-        Certification.find(e+"z"){
-
-        }
-
-        Certification.find(e){
-
-        }
     }
 }
